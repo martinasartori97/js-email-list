7f692938db2551d7c8aac8f14399e915b3cf3e60
martinasartori97 <martinasartori9@gmail.com>
2024-10-17 15:57:03 +0200

    edit js

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 console.log("hello");
+
+const email = [
+    "mario.rossi@gmail.com",
+    "giulia.bianchi@yahoo.com",
+    "marco.verdi@outlook.com",
+    "sara.neri@icloud.com",
+    "luca.ferri@hotmail.com",
+    "chiara.romano@mail.com",
+    "antonio.gallo@live.com",
+    "elena.moretti@protonmail.com",
+    "francesco.rinaldi@zoho.com",
+    "laura.conti@aol.com"
+];
+
+console.log(email);
+
+
+.get(https://flynn.boolean.careers/exercises/api/random/mail)
+.then((response)=>{
+console.log(response.data)
+})
