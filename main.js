2bbeff0dbf220dd3c4c9595533c1ef4f116a30e6
martinasartori97 <martinasartori9@gmail.com>
2024-10-17 16:19:41 +0200

    edit js

--- main.js
+++ main.js
@@ -16,7 +16,9 @@ const email = [
 console.log(email);
 
 
-.get(https://flynn.boolean.careers/exercises/api/random/mail)
-.then((response) => {
-    console.log(response.data)
-})
+
+axios
+    .get("https://flynn.boolean.careers/exercises/api/random/mail")
+    .then((response) => {
+        console.log(response.data)
+    })
